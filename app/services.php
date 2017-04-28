10b76a6332022cb9e34652da9289d4b3ec28d534
Alessandro Mangiameli <alessandro.mangiameli@arcww.leoburnett.it>
2017-04-28 15:17:41 +0200

    fix twig cache

--- app/services.php
+++ app/services.php
@@ -4,7 +4,8 @@ $container = $app->getContainer();
 // Register component on container
 $container['view'] = function ($container) {
     $view = new \Slim\Views\Twig(__DIR__ . '/views', [
-        'cache' => __DIR__ . '/../storage/views'
+        'cache' => __DIR__ . '/../storage/views',
+        'auto_reload' => true
     ]);
     
     // Instantiate and add Slim specific extension
